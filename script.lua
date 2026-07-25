343517908566ba76cacbefaa0cd55199964af299
jaxsy-likkee <zapasak71@gmail.com>
2026-07-25 17:51:11 +0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -11,7 +11,7 @@ scrFrame.Name = "scrFrame"
 scrFrame.Parent = MainGui
 scrFrame.Size = UDim2.new(0, 250, 0, 150)
 scrFrame.Position = UDim2.new(0, 10, 0, 10)
-scrFrame.CanvasSize = UDim2.new(0, 0, 0, 520)
+scrFrame.CanvasSize = UDim2.new(0, 0, 0, 365)
 scrFrame.ScrollBarThickness = 10
 scrFrame.BackgroundTransparency = 0
 scrFrame.BackgroundColor3 = Color3.fromRGB(20, 20, 20)
@@ -23,8 +23,8 @@ corner.Parent = scrFrame
 
 local scrollBar = scrFrame:FindFirstChildWhichIsA("ScrollBar")
 if scrollBar then
-	scrollBar.BackgroundTransparency = 1
-	scrollBar.Size = UDim2.new(0, 0, 1, 0)
+    scrollBar.BackgroundTransparency = 1
+    scrollBar.Size = UDim2.new(0, 0, 1, 0)
 end
 
 
@@ -61,12 +61,12 @@ yarhmCorner.Parent = yarhmButton
 
 -- Click
 yarhmButton.MouseButton1Click:Connect(function()
-	local success, err = pcall(function()
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Joystickplays/psychic-octo-invention/main/yarhm.lua", false))()
-	end)
-	if not success then
-		warn("Launch error:", err)
-	end
+    local success, err = pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Joystickplays/psychic-octo-invention/main/yarhm.lua", false))()
+    end)
+    if not success then
+        warn("Launch error:", err)
+    end
 end)
 
 
@@ -76,7 +76,7 @@ instructionLabel1.Name = "Instruction_YARHM"
 instructionLabel1.Parent = scrFrame
 instructionLabel1.Text = "YARHM: Menu for MM2, Forsaken, Flee The Facility, Universal."
 instructionLabel1.Size = UDim2.new(1, -20, 0, 70)
-instructionLabel1.Position = UDim2.new(0, 10, 0, 80)
+instructionLabel1.Position = UDim2.new(0, 10, 0, 85)
 instructionLabel1.BackgroundTransparency = 1
 instructionLabel1.TextColor3 = Color3.fromRGB(220, 220, 220)
 instructionLabel1.Font = Enum.Font.Gotham
@@ -92,7 +92,7 @@ animalhospButton.Name = "ANHS"
 animalhospButton.Parent = scrFrame
 animalhospButton.Text = "Animal Hospital"
 animalhospButton.Size = UDim2.new(1, -20, 0, 50)
-animalhospButton.Position = UDim2.new(0, 10, 0, 190)
+animalhospButton.Position = UDim2.new(0, 10, 0, 155)
 animalhospButton.BackgroundColor3 = Color3.fromRGB(35, 142, 185)
 animalhospButton.TextColor3 = Color3.new(1, 1, 1)
 animalhospButton.Font = Enum.Font.GothamBold
@@ -105,12 +105,12 @@ animalHospitalCorner.Parent = animalhospButton
 
 -- Click
 animalhospButton.MouseButton1Click:Connect(function()
-	local success, err = pcall(function()
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/najzgaming/Animal-hospital/refs/heads/main/AHA.lua"))()
-	end)
-	if not success then
-		warn("Launch error:", err)
-	end
+    local success, err = pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/najzgaming/Animal-hospital/refs/heads/main/AHA.lua"))()
+    end)
+    if not success then
+        warn("Launch error:", err)
+    end
 end)
 
 -- Instruction
@@ -119,7 +119,7 @@ instructionLabel2.Name = "Instruction_ANHS"
 instructionLabel2.Parent = scrFrame
 instructionLabel2.Text = "Animal Hospital: ESP, anomaly display, speed boost."
 instructionLabel2.Size = UDim2.new(1, -20, 0, 70)
-instructionLabel2.Position = UDim2.new(0, 10, 0, 250)
+instructionLabel2.Position = UDim2.new(0, 10, 0, 190)
 instructionLabel2.BackgroundTransparency = 1
 instructionLabel2.TextColor3 = Color3.fromRGB(220, 220, 220)
 instructionLabel2.Font = Enum.Font.Gotham
@@ -135,7 +135,7 @@ espButton.Name = "ESP"
 espButton.Parent = scrFrame
 espButton.Text = "Start ESP"
 espButton.Size = UDim2.new(1, -20, 0, 50)
-espButton.Position = UDim2.new(0, 10, 0, 340)
+espButton.Position = UDim2.new(0, 10, 0, 260)
 espButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 espButton.TextColor3 = Color3.fromRGB(17, 17, 17)
 espButton.Font = Enum.Font.GothamBold
@@ -148,12 +148,12 @@ espCorner.Parent = espButton
 
 -- Click
 espButton.MouseButton1Click:Connect(function()
-	local success, err = pcall(function()
-		loadstring(game:HttpGet("https://pastebin.com/raw/CmyR7fct"))()
-	end)
-	if not success then
-		warn("Launch error:", err)
-	end
+    local success, err = pcall(function()
+        loadstring(game:HttpGet("https://pastebin.com/raw/CmyR7fct"))()
+    end)
+    if not success then
+        warn("Launch error:", err)
+    end
 end)
 
 
@@ -163,7 +163,7 @@ instructionLabel3.Name = "Instruction_ESP"
 instructionLabel3.Parent = scrFrame
 instructionLabel3.Text = "ESP: A script for observing players through walls. It can't be disabled unless you restart the game."
 instructionLabel3.Size = UDim2.new(1, -20, 0, 70)
-instructionLabel3.Position = UDim2.new(0, 10, 0, 400)
+instructionLabel3.Position = UDim2.new(0, 10, 0, 300)
 instructionLabel3.BackgroundTransparency = 1
 instructionLabel3.TextColor3 = Color3.fromRGB(220, 220, 220)
 instructionLabel3.Font = Enum.Font.Gotham
